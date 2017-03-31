f351370659d60bc87f479ee9be350ad0d9c412c9
tomanistor <toma.nistor@gmail.com>
2017-03-31 00:51:09 -0700

    Added hamburger menu

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
 
     // Nav starts at bottom then is fixed to top
+    // Logo and hamburger menus fade in and out
     $(window).scroll(function() {
         if( $(this).scrollTop() > $(this).height() - $("nav").height() ) {
             $("nav").addClass("nav-fixed");
             $("nav > div").first().fadeIn();
-
+            $("nav > div").last().fadeIn();
+            // $("nav > div").last().removeClass("hidden");
         } else {
             $("nav").removeClass("nav-fixed");
             $("nav > div").first().fadeOut();
+            $("nav > div").last().fadeOut();
+            // $("nav > div").last().addClass("hidden");
         }
     });
 
+    $(".nav-icon").click(function(){
+        $(".nav-full").toggleClass("active");
+        $(".page").toggleClass("active");
+    });
 });
