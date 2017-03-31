62dc4e76e9545c51ec60924899e49461667f586e
tomanistor <toma.nistor@gmail.com>
2017-03-31 03:53:43 -0700

    Fixed hamburger menu display

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
     $(window).scroll(function() {
         if( $(this).scrollTop() > $(this).height() - $("nav").height() ) {
             $("nav").addClass("nav-fixed");
-            $("nav > div.logo").find("a").fadeIn();
-            $("nav > div.nav-toggle").find("a").fadeIn();
+            $("nav > div.logo").fadeIn();
+            $("nav > div.nav-toggle").fadeIn();
         } else {
             $("nav").removeClass("nav-fixed");
-            $("nav > div.logo").find("a").fadeOut();
-            $("nav > div.nav-toggle").find("a").fadeOut();
+            $("nav > div.logo").fadeOut();
+            $("nav > div.nav-toggle").fadeOut();
         }
     });
 
