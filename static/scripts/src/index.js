27ca06e90c338fe513d1adb6ee7b610d8f4d2e80
Toma Nistor <toma.nistor@gmail.com>
2018-03-06 23:09:12 -0800

    Moved scripts from footer into body-bottom, fixed issue #36 - scrolling when full screen nav is active

--- static/scripts/src/index.js
+++ static/scripts/src/index.js
@@ -61,6 +61,15 @@ var toggle = function(el) {
     });
   });
 
+  // Disable scroll when full screen nav is open
+  $('body').addEventListener('click', function() {
+    if ($('.nav-full').classList.contains('active')) {
+      $('html').style.overflowY = 'hidden';
+    } else {
+      $('html').style.overflowY = 'scroll';
+    }
+  });
+
 }());
 
 // Mobile browsers viewport height bug fix
