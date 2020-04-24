b3bbb886df812caadec539cad8b00fcacb73d7e3
Catalin Apostu <dev.catalin.apostu@gmail.com>
2020-04-24 22:33:05 +0200

    [Fix] Nav icon does not update back to the menu icon on nav item click
    * when clicking on an item in the full screen nav, the nav would close, but the nav icon would not update back to the menu icon and still show the X icon

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -30,30 +30,33 @@
     }
   }
 
-  // Full screen nav open on click
+  // Full screen nav open/close on nav icon click
   $('.nav-icon').addEventListener('click', function() {
+    toggleNav()
+  })
+
+  // Full screen nav close on nav item click
+  $$('.nav-full a').forEach(function(links) {
+    links.addEventListener('click', function() {
+      toggleNav()
+    })
+  })
+
+  // Full screen nav toggle
+  function toggleNav() {
     $$('.nav-full, main').forEach(function(el) {
       el.classList.toggle('active')
     })
     if (menuActive) {
-      this.querySelector('img:nth-of-type(1)').style.display = 'inline-block'
-      this.querySelector('img:nth-of-type(2)').style.display = 'none'
+      $('.nav-icon').querySelector('img:nth-of-type(1)').style.display = 'inline-block'
+      $('.nav-icon').querySelector('img:nth-of-type(2)').style.display = 'none'
       menuActive = false
     } else {
-      this.querySelector('img:nth-of-type(1)').style.display = 'none'
-      this.querySelector('img:nth-of-type(2)').style.display = 'inline-block'
+      $('.nav-icon').querySelector('img:nth-of-type(1)').style.display = 'none'
+      $('.nav-icon').querySelector('img:nth-of-type(2)').style.display = 'inline-block'
       menuActive = true
     }
-  })
-
-  // Full screen nav close on click
-  $$('.nav-full a').forEach(function(links) {
-    links.addEventListener('click', function() {
-      $$('.nav-full, main').forEach(function(el) {
-        el.classList.toggle('active')
-      })
-    })
-  })
+  }
 
   // Fix logoBig drawing over nav when click on logoSmall while nav open
   $('.logo').addEventListener('click', function() {
